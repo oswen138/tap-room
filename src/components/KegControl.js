8593f2abcab9c36c5b058713b02ea4274e84a405
ophelia swen <ophelia.swen@gmail.com>
2020-11-20 11:02:44 -0800

    add event handler to component, lesson 22

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -12,14 +12,17 @@ class KegControl extends React.Component {
   }
   render(){
     let currentlyVisibleState = null;
+    let addKegButton = null;
     if (this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm />
     } else {
       currentlyVisibleState = <KegList />
+      addKegButton = <button onClick={this.handleClick}>Add keg</button>
     }
     return (
       <React.Fragment>
         {currentlyVisibleState}
+        {addKegButton}
       </React.Fragment>
     );
   }
