c1eed4109dc464eb203eb29af944b2da5cbe0f27
ophelia swen <ophelia.swen@gmail.com>
2020-11-20 16:46:12 -0800

    modify component diagram

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -20,7 +20,8 @@ class KegControl extends React.Component {
     if (this.state.selectedKeg != null) {
       this.setState({
         formVisibleOnPage: false,
-        selectedKeg: null
+        selectedKeg: null,
+        editing: false
       });
     } else {
       this.setState(prevState => ({
